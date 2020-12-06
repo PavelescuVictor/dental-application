8e41ad485a8b022464589834363ab9169c63849c
PavelescuVictor <pavelescu.victor97@gmail.com>
2020-12-07 00:35:48 +0200

    Modifying django settings

--- dentalapp-backend/dentalapp/dentalapp/settings.py
+++ dentalapp-backend/dentalapp/dentalapp/settings.py
@@ -124,7 +124,7 @@ DATABASES = {
         'NAME': 'DentalApplication',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': 'localhost',
+        'HOST': 'dentalapppostgresql',
         'PORT': '5432',
     }
 }
